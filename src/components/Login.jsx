4b60537f935c41fce8e4b70c9a2b1493f9a98bd3
blueberryenjoyer <phoenix.wheatley@gmail.com>
2023-06-14 12:12:35 -0400

    made logins work with new api

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,9 +7,36 @@ export default function Login() {
         document.getElementById("authresult").innerHTML = awoo
     }
 
+    const login = async () => {
+
+        try {
+            const response = await fetch(`https://strangers-things.herokuapp.com/api/2209-FTB-ET-WEB-FT/users/login`, {
+                method: "POST",
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    user: {
+                        username: `${(document.getElementById('signupUsername')).value}`,
+                        password: `${(document.getElementById('signupPassword')).value}`
+                    }
+                })
+            });
+            const result = await response.json();
+            console.log(result);
+            localStorage.setItem("token", result.data.token);
+
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
     return (
         <>
-            <p onClick={alertAuth} className='makeTheseADifferentColorPlease'>click me to verify login</p>
+            <input id='signupUsername' type="text" placeholder="username" />
+            <input id='signupPassword' type="text" placeholder="password" />
+            <p onClick={() => login()} className='makeTheseADifferentColorPlease'>login</p>
+            <p onClick={() => alertAuth(localStorage.getItem('token'))} className='makeTheseADifferentColorPlease'>click me to verify login</p>
             <p id="authresult"></p>
         </>
     )
